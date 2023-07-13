5c1f2085ebe67510cb2ca2a43f9feff37ec8448e
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-07-14 00:34:31 +0300

    Updated Lambda and JS to cater for CORS

--- js/script.js
+++ js/script.js
@@ -51,6 +51,32 @@
 // }
 // updateCounter();
 
+// JS code for visitor counter - has CORS issue
+// const counter = document.querySelector(".counter-number");
+
+// async function updateCounter() {
+//     try {
+//         let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+//         let data = await response.json();
+
+//         if (data && data['Visit_Count']) {
+//             var visitCount = data['Visit_Count'];
+//             counter.innerHTML = `Views: ${visitCount}`;
+//         } else if (data && data['error']) {
+//             var errorMessage = data['error'];
+//             document.getElementById('counter-error').innerHTML = errorMessage;
+//         } else {
+//             document.getElementById('counter-error').innerHTML = 'Unknown error occurred.';
+//         }
+//     } catch (error) {
+//         console.error("Error:", error);
+//         document.getElementById('counter-error').innerHTML = 'Error occurred while fetching counter data.';
+//     }
+// }
+
+// updateCounter();
+
+
 // JS code for visitor counter
 const counter = document.querySelector(".counter-number");
 

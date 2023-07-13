73a393b742c405c5992476883ae3940c79b66fc1
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-07-14 01:16:51 +0300

    New counter

--- js/script.js
+++ js/script.js
@@ -25,26 +25,44 @@
 
 // JS code version 2
 // JS code for visitor counter
-const counter = document.getElementById("Views");
+// const counter = document.getElementById("Views");
 
+// async function updateCounter() {
+//     try {
+//         let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+//         let data = await response.json();
+
+//         if (data && data['Visit_Count']) {
+//             var visitCount = data['Visit_Count'];
+//             counter.innerHTML = visitCount;
+//         } else if (data && data['error']) {
+//             var errorMessage = data['error'];
+//             console.error("Error:", errorMessage);
+//         } else {
+//             console.error("Unknown error occurred.");
+//         }
+//     } catch (error) {
+//         console.error("Error:", error);
+//     }
+// }
+
+// updateCounter();
+
+// JS code version 3
 async function updateCounter() {
     try {
-        let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
-        let data = await response.json();
-
-        if (data && data['Visit_Count']) {
-            var visitCount = data['Visit_Count'];
-            counter.innerHTML = visitCount;
-        } else if (data && data['error']) {
-            var errorMessage = data['error'];
-            console.error("Error:", errorMessage);
-        } else {
-            console.error("Unknown error occurred.");
-        }
+      let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+      let data = await response.json();
+  
+      if (data && data['Visit_Count']) {
+        var visitCount = data['Visit_Count'];
+        document.getElementById('counter').textContent = visitCount;
+      } else {
+        console.error("Unknown error occurred.");
+      }
     } catch (error) {
-        console.error("Error:", error);
+      console.error("Error:", error);
     }
-}
-
-updateCounter();
-
+  }
+  
+  updateCounter();  

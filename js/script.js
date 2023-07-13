c95c855acb15392a26acea770e466a575808dd48
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-07-14 01:00:01 +0300

    updated the JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 // JS code for visitor counter
-const counter = document.querySelector(".counter-number");
+// const counter = document.querySelector(".counter-number");
+
+// async function updateCounter() {
+//     try {
+//         let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+//         let data = await response.json();
+
+//         if (data && data['Visit_Count']) {
+//             var visitCount = data['Visit_Count'];
+//             counter.innerHTML = `Views: ${visitCount}`;
+//         } else if (data && data['error']) {
+//             var errorMessage = data['error'];
+//             document.getElementById('counter-error').innerHTML = errorMessage;
+//         } else {
+//             document.getElementById('counter-error').innerHTML = 'Unknown error occurred.';
+//         }
+//     } catch (error) {
+//         console.error("Error:", error);
+//         document.getElementById('counter-error').innerHTML = 'Error occurred while fetching counter data.';
+//     }
+// }
+
+// updateCounter();
+
+// JS code version 2
+// JS code for visitor counter
+const counter = document.getElementById("Views");
 
 async function updateCounter() {
     try {
@@ -8,17 +34,17 @@ async function updateCounter() {
 
         if (data && data['Visit_Count']) {
             var visitCount = data['Visit_Count'];
-            counter.innerHTML = `Views: ${visitCount}`;
+            counter.innerHTML = visitCount;
         } else if (data && data['error']) {
             var errorMessage = data['error'];
-            document.getElementById('counter-error').innerHTML = errorMessage;
+            console.error("Error:", errorMessage);
         } else {
-            document.getElementById('counter-error').innerHTML = 'Unknown error occurred.';
+            console.error("Unknown error occurred.");
         }
     } catch (error) {
         console.error("Error:", error);
-        document.getElementById('counter-error').innerHTML = 'Error occurred while fetching counter data.';
     }
 }
 
 updateCounter();
+

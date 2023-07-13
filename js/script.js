c0c8796b1b103b3e71be9e954b50fe654b45c6b7
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-07-14 00:30:32 +0300

    Updated Lambda function

--- js/script.js
+++ js/script.js
@@ -42,13 +42,36 @@
 //   </script> -->
 
 
+// JS code for visitor counter - works but dowsnt display
+// const counter = document.querySelector(".counter-number");
+// async function updateCounter() {
+//     let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+//     let data = await response.json();
+//     counter.innerHTML = ` Views: ${data}`; //Views will be the data returned from the dynamoDB table
+// }
+// updateCounter();
+
 // JS code for visitor counter
 const counter = document.querySelector(".counter-number");
+
 async function updateCounter() {
-    let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`; //Views will be the data returned from the dynamoDB table
+    try {
+        let response = await fetch("https://gcvbryl5r4exmiel6oba4r6ql40ktdqx.lambda-url.us-east-1.on.aws/");
+        let data = await response.json();
+
+        if (data && data['Visit_Count']) {
+            var visitCount = data['Visit_Count'];
+            counter.innerHTML = `Views: ${visitCount}`;
+        } else if (data && data['error']) {
+            var errorMessage = data['error'];
+            document.getElementById('counter-error').innerHTML = errorMessage;
+        } else {
+            document.getElementById('counter-error').innerHTML = 'Unknown error occurred.';
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        document.getElementById('counter-error').innerHTML = 'Error occurred while fetching counter data.';
+    }
 }
 
 updateCounter();
-
